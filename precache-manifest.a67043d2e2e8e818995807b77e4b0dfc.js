3e12b8b30b169fcda3213909d6cdcc556e571c2d
Gamer1982 <57429718+Gamer1982@users.noreply.github.com>
2020-11-17 18:57:50 +0300

    test1

---
+++ precache-manifest.a67043d2e2e8e818995807b77e4b0dfc.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb96daf02e03102bef6d",
+    "url": "/css/app.31257f0a.css"
+  },
+  {
+    "revision": "38ca9e2302c8793c93f854e8595eed78",
+    "url": "/fonts/OpenSans-Regular.38ca9e23.eot"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/fonts/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/fonts/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "7106bb83a0e767e12659de9cfba3926d",
+    "url": "/fonts/OpenSans-SemiBold.7106bb83.woff2"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/fonts/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "e0d5280b186197bd2a77b9babfe5506d",
+    "url": "/fonts/OpenSans-SemiBold.e0d5280b.eot"
+  },
+  {
+    "revision": "fe241c6c4f5a190e821b7c03e337d756",
+    "url": "/fonts/OpenSans-SemiBold.fe241c6c.woff"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "/fonts/Overpass-SemiBold.17a1468e.ttf"
+  },
+  {
+    "revision": "7c33a1ba0484c4bc53d7aec92e3b6561",
+    "url": "/fonts/Overpass-SemiBold.7c33a1ba.eot"
+  },
+  {
+    "revision": "c33630c38857a6d91c7926e985bb0755",
+    "url": "/fonts/Overpass-SemiBold.c33630c3.woff"
+  },
+  {
+    "revision": "333ee0ee5989e593812c23ca2dd7bc24",
+    "url": "/fonts/PTSans-Bold.333ee0ee.ttf"
+  },
+  {
+    "revision": "cb6e4ea40efb1c0e1dc7c611fb4e4b7d",
+    "url": "/fonts/PTSans-Bold.cb6e4ea4.woff"
+  },
+  {
+    "revision": "d2f4967140f3411eb9f345fb72cfd4d3",
+    "url": "/fonts/PTSans-Bold.d2f49671.eot"
+  },
+  {
+    "revision": "1b01cdd6528a9aa5870efbb807c8092f",
+    "url": "/fonts/PTSans-Regular.1b01cdd6.woff"
+  },
+  {
+    "revision": "2cbd87da7ce7cbf497ea98c1aa3f0520",
+    "url": "/fonts/PTSans-Regular.2cbd87da.eot"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/fonts/PTSans-Regular.4ea26cd5.ttf"
+  },
+  {
+    "revision": "15b23995d7bca07a93afb7194851668f",
+    "url": "/fonts/SourceSansPro-Regular.15b23995.eot"
+  },
+  {
+    "revision": "72f2b81d51ae9bbe7c3502e9af2dcf50",
+    "url": "/fonts/SourceSansPro-Regular.72f2b81d.woff"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/fonts/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "bbbd47d2660d3547e98a864324874080",
+    "url": "/img/OpenSans-Regular.bbbd47d2.svg"
+  },
+  {
+    "revision": "dd308ca05314118f5410b2675ef2c2c2",
+    "url": "/img/OpenSans-SemiBold.dd308ca0.svg"
+  },
+  {
+    "revision": "6164b38bccb6bdb6f99b7dabf57fff9d",
+    "url": "/img/Overpass-SemiBold.6164b38b.svg"
+  },
+  {
+    "revision": "349ad6acaa52f5afaabd05c094e6f400",
+    "url": "/img/PTSans-Bold.349ad6ac.svg"
+  },
+  {
+    "revision": "2a54b34748143e9516df4fc88eff3461",
+    "url": "/img/PTSans-Regular.2a54b347.svg"
+  },
+  {
+    "revision": "40109fef3eb0021f30d53183a771f4a6",
+    "url": "/img/SourceSansPro-Regular.40109fef.svg"
+  },
+  {
+    "revision": "a7cfdd5d656800aa173481d4f856d9e3",
+    "url": "/img/banner-photo_1024.a7cfdd5d.jpg"
+  },
+  {
+    "revision": "fb31e73487a9fd5d290b0d8da36e5626",
+    "url": "/img/banner-photo_1366.fb31e734.jpg"
+  },
+  {
+    "revision": "2a5a1fa65783f409680492a4dc98709f",
+    "url": "/img/banner-photo_1920.2a5a1fa6.jpg"
+  },
+  {
+    "revision": "762011f0d3c57b18a1faaf909f95c30d",
+    "url": "/img/banner-photo_768.762011f0.jpg"
+  },
+  {
+    "revision": "32af61b74ae2dd2cd59e8c1004a41155",
+    "url": "/img/logo.32af61b7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/img/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "a33f2d3cee0ddafd303461d319e9a2ab",
+    "url": "/img/photo-cover.a33f2d3c.svg"
+  },
+  {
+    "revision": "889821ffc426fc55fb27f5377b8531cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb96daf02e03102bef6d",
+    "url": "/js/app.dcbfef14.js"
+  },
+  {
+    "revision": "625668555eaec029f82e",
+    "url": "/js/chunk-vendors.af58867b.js"
+  },
+  {
+    "revision": "a4538c1ccfeaec9e79be49e6042cf296",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
